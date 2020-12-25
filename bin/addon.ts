65e2185f38d5ca083963400853ae4fe7956313f9
KYDronePilot <kydronepilotdev@gmail.com>
2020-12-24 20:22:08 -0500

    Removed unneeded files

--- bin/addon.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs';
-import * as path from 'path';
-
-['desktopwallpaper'].forEach((name: string) => {
-  const src: string = path.join(__dirname, `../build/Release/${name}.node`);
-  const dest: string = path.join(__dirname, `../src/addons/${name}.node`);
-
-  fs.copyFileSync(src, dest);
-});
